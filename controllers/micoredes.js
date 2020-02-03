5b78d0bdf12ca4c4c5ac08dd4374b64be9b39db4
CastroC4 <Castro_2003_007@hotmail.com>
2020-02-03 00:16:09 -0500

    get image on other side

--- controllers/micoredes.js
+++ controllers/micoredes.js
@@ -3,9 +3,30 @@ const prisma = new Prisma()
 const express = require('express')
 const router = express.Router()
 
-router.post('/redes', async function(req, res) {
-  const redes = await prisma.microRedes2.findMany()
+router.get('/redes', async function(req, res) {
+  const redes = await prisma.microRedes2.findMany({
+    select: {
+      id,
+      color,
+      familia,
+      n_de_paredes_de_la_espora,
+      pais,
+      tamanio_um,
+      textura_de_la_espora,
+      tombre_cientifico,
+      informacion_de_la_especie
+    }
+  })
   res.json(redes)
+})
+
+router.get('/redes/image/:id', async function(req, res) {
+  const id = req.params.id
+  const image = await prisma.microRedes2.findOne({
+    where: { id },
+    select: { foto }
+  })
+  return image.foto
 })
 
 module.exports = router
