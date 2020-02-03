a6fb8e4ce22552a56e56b8e8684d39d9453d7e7d
CastroC4 <Castro_2003_007@hotmail.com>
2020-02-03 00:40:39 -0500

    selct fix x2

--- controllers/micoredes.js
+++ controllers/micoredes.js
@@ -6,7 +6,16 @@ const router = express.Router()
 router.get('/redes', async function(req, res) {
   const redes = await prisma.microRedes2.findMany({
     select: {
-      foto: false
+      id: true,
+      color: true,
+      foto: false,
+      familia: true,
+      n_de_paredes_de_la_espora: true,
+      pais: true,
+      tamanio_um: true,
+      textura_de_la_espora: true,
+      tombre_cientifico: true,
+      informacion_de_la_especie: true
     }
   })
   res.json(redes)
@@ -16,7 +25,18 @@ router.get('/redes/image/:id', async function(req, res) {
   const id = req.params.id
   const image = await prisma.microRedes2.findOne({
     where: { id },
-    select: { foto: true }
+    select: {
+      id: false,
+      color: false,
+      foto: true,
+      familia: false,
+      n_de_paredes_de_la_espora: false,
+      pais: false,
+      tamanio_um: false,
+      textura_de_la_espora: false,
+      tombre_cientifico: false,
+      informacion_de_la_especie: false
+    }
   })
   return res.json({ data: image.foto })
 })
